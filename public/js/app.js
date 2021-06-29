94b17de3e265b7a0bf66425f188f62cd11c3c2c3
Reeetesh <riteshsahu1560@gmail.com>
2021-06-29 10:58:23 +0530

    remove console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript file is loaded')
-
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
